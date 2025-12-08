c9cad484767f7ff952e4a67334f56e693cdbb46b
Andrew Luther <90206207+AndrewLuther@users.noreply.github.com>
2025-12-08 10:27:59 -0500

    Tried to add some stuff for rooms, work in progress

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const wss = new WebSocketServer({ noServer: true });
 const app = express()
 const port = 3000
 
+const rooms = new Map();
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(import.meta.dirname, "home.html"))
 })
@@ -16,15 +18,26 @@ app.get('/game/:id', (req, res) => {
 })
 
 app.post('/api/create-game', (req, res) => {
+
+    const room_id = createRoomId()
     res.json({
-        id: "foo"
+        id: room_id
     })
+
+    if (!rooms.get(room_id)) {
+      rooms.set(room_id, new Set())
+    }
 })
 
 wss.on("connection", (ws, request) => {
 
   ws.on("message", msg => {
-    console.log(msg.toString())
+    msg = JSON.parse(msg.toString())
+    const room_clients = rooms.get(msg.room_id)
+    room_clients.add(msg.client_id)
+    rooms.set(msg.room_id, room_clients)
+
+    console.log(rooms)
   });
 
   ws.on("close", () => {
@@ -41,3 +54,7 @@ server.on("upgrade", (request, socket, head) => {
     wss.emit("connection", ws, request);
   });
 });
+
+function createRoomId() {
+  return "foo"
+}
